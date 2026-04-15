8e24889df76c79e7909fc153a2294d6bb083fc35
2025050018 <koreaair0916@gmail.com>
2026-04-15 15:52:21 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -12,14 +12,14 @@ int main() {
         count++;
 
         if (number > answer) {
-            printf("%d>?\n", number);
+            printf("%d?> %d 보다 낮습니다\n", number, number);
         }
         else if (number < answer) {
-            printf("%d<?\n", number);
+            printf("%d<? %d 보다 높습니다\n", number, number);
         }
         else {
-            printf("%d==?\n", number);
-            printf("%d\n", answer, count);
+            printf("%d==? %d 정답입니다\n", number, number);
+            printf("%d -> 시도횟수는 %d회\n", answer, count);
         }
 
     } while (number != answer);
